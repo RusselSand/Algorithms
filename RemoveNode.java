3cba711a68607bed21f9819d196c0ecc1a6c75ce
Valeriia Ziukina <sand16031986@gmail.com>
2023-11-15 09:15:05 +0100

    added Node to code

--- RemoveNode.java
+++ RemoveNode.java
@@ -81,4 +81,40 @@ public class RemoveNode {
         assert newHead.getRight() == node5;
         assert newHead.getRight().getValue() == 8;
     }
+
+    public static class Node {
+        private int value;
+        private Node left;
+        private Node right;
+
+        Node(Node left, Node right, int value) {
+            this.left = left;
+            this.right = right;
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+    }
 }
